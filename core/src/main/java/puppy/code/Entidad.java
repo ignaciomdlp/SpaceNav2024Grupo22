135741fbcb75a41eb9b901ed1d16048c4dffdd49
Anxlytic <benjamin.leiva.c@mail.pucv.cl>
2024-10-29 14:45:39 -0300

    Creacion Clase Abstracta Entidad que contiene metodos comunes de nave4 y ball2 en donde las dos clases extienden de entidad

---
+++ core/src/main/java/puppy/code/Entidad.java
@@ -0,0 +1,76 @@
+package puppy.code;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+public abstract class Entidad {
+    protected int x;
+    protected int y;
+    protected int xSpeed;
+    protected int ySpeed;
+    protected Sprite sprite;
+
+    public Entidad(int x, int y, int size, int xSpeed, int ySpeed, Texture texture) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+        this.sprite = new Sprite(texture);
+        this.sprite.setPosition(x, y);
+    }
+
+    public void update() {
+        x += xSpeed;
+        y += ySpeed;
+        handleBorders();
+        sprite.setPosition(x, y);
+    }
+
+    protected abstract void handleBorders();
+
+    public Rectangle getArea() {
+        return sprite.getBoundingRectangle();
+    }
+
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+
+    public int getXSpeed() {
+        return xSpeed;
+    }
+
+    public int getYSpeed() {
+        return ySpeed;
+    }
+
+    public void setXSpeed(int xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public void setYSpeed(int ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+        sprite.setX(x); // Actualiza la posición del sprite
+    }
+
+    public void setY(int y) {
+        this.y = y;
+        sprite.setY(y); // Actualiza la posición del sprite
+    }
+
+
+}
+
--- core/src/main/java/puppy/code/Nave4.java
+++ core/src/main/java/puppy/code/Nave4.java
@@ -4,124 +4,102 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 
-
-
-public class Nave4 {
-	
-	private boolean destruida = false;
+public class Nave4 extends Entidad {
+    private boolean destruida = false;
     private int vidas = 3;
-    private float xVel = 0;
-    private float yVel = 0;
-    private Sprite spr;
+    private boolean herido = false;
+    private int tiempoHeridoMax = 50;
+    private int tiempoHerido;
     private Sound sonidoHerido;
     private Sound soundBala;
     private Texture txBala;
-    private boolean herido = false;
-    private int tiempoHeridoMax=50;
-    private int tiempoHerido;
-    
-    public Nave4(int x, int y, Texture tx, Sound soundChoque, Texture txBala, Sound soundBala) {
-    	sonidoHerido = soundChoque;
-    	this.soundBala = soundBala;
-    	this.txBala = txBala;
-    	spr = new Sprite(tx);
-    	spr.setPosition(x, y);
-    	//spr.setOriginCenter();
-    	spr.setBounds(x, y, 45, 45);
 
+    public Nave4(int x, int y, Texture tx, Sound soundChoque, Texture txBala, Sound soundBala) {
+        super(x, y, 45, 0, 0, tx);
+        this.sonidoHerido = soundChoque;
+        this.soundBala = soundBala;
+        this.txBala = txBala;
     }
-    public void draw(SpriteBatch batch, PantallaJuego juego){
-        float x =  spr.getX();
-        float y =  spr.getY();
-        if (!herido) {
-	        // que se mueva con teclado
-	        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) xVel--;
-	        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) xVel++;
-        	if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) yVel--;     
-	        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) yVel++;
-        	
-	     /*   if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) spr.setRotation(++rotacion);
-	        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) spr.setRotation(--rotacion);
-	        
-	        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-	        	xVel -=Math.sin(Math.toRadians(rotacion));
-	        	yVel +=Math.cos(Math.toRadians(rotacion));
-	        	System.out.println(rotacion+" - "+Math.sin(Math.toRadians(rotacion))+" - "+Math.cos(Math.toRadians(rotacion))) ;    
-	        }
-	        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-	        	xVel +=Math.sin(Math.toRadians(rotacion));
-	        	yVel -=Math.cos(Math.toRadians(rotacion));
-	        	     
-	        }*/
-	        
-	        // que se mantenga dentro de los bordes de la ventana
-	        if (x+xVel < 0 || x+xVel+spr.getWidth() > Gdx.graphics.getWidth())
-	        	xVel*=-1;
-	        if (y+yVel < 0 || y+yVel+spr.getHeight() > Gdx.graphics.getHeight())
-	        	yVel*=-1;
-	        
-	        spr.setPosition(x+xVel, y+yVel);   
-         
- 		    spr.draw(batch);
-        } else {
-           spr.setX(spr.getX()+MathUtils.random(-2,2));
- 		   spr.draw(batch); 
- 		  spr.setX(x);
- 		   tiempoHerido--;
- 		   if (tiempoHerido<=0) herido = false;
- 		 }
-        // disparo
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {         
-          Bullet  bala = new Bullet(spr.getX()+spr.getWidth()/2-5,spr.getY()+ spr.getHeight()-5,0,3,txBala);
-	      juego.agregarBala(bala);
-	      soundBala.play();
+
+    @Override
+    protected void handleBorders() {
+        if (x + xSpeed < 0 || x + xSpeed + sprite.getWidth() > Gdx.graphics.getWidth()) {
+            xSpeed *= -1;
         }
-       
+        if (y + ySpeed < 0 || y + ySpeed + sprite.getHeight() > Gdx.graphics.getHeight()) {
+            ySpeed *= -1;
+        }
     }
-      
+
+    public void draw(SpriteBatch batch, PantallaJuego juego) {
+        if (!herido) {
+            // Control de movimiento con teclado
+            if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) setXSpeed(getXSpeed() - 1);
+            if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) setXSpeed(getXSpeed() + 1);
+            if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) setYSpeed(getYSpeed() - 1);
+            if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) setYSpeed(getYSpeed() + 1);
+
+            super.update();
+            draw(batch);
+        } else {
+            // Efecto de daño
+            setX(getX() + MathUtils.random(-2, 2));
+            sprite.draw(batch);
+            tiempoHerido--;
+            if (tiempoHerido <= 0) herido = false;
+        }
+
+        // Disparo
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            Bullet bala = new Bullet(getX() + sprite.getWidth() / 2 - 5, getY() + sprite.getHeight() - 5, 0, 3, txBala);
+            juego.agregarBala(bala);
+            sonidoHerido.play();  // Asumiendo que el sonido de disparo se almacena en sonidoHerido
+        }
+    }
+
     public boolean checkCollision(Ball2 b) {
-        if(!herido && b.getArea().overlaps(spr.getBoundingRectangle())){
-        	// rebote
-            if (xVel ==0) xVel += b.getXSpeed()/2;
-            if (b.getXSpeed() ==0) b.setXSpeed(b.getXSpeed() + (int)xVel/2);
-            xVel = - xVel;
+        if (!herido && b.getArea().overlaps(getArea())) {
+            // Rebote
+            if (getXSpeed() == 0) setXSpeed(b.getXSpeed() / 2);
+            if (b.getXSpeed() == 0) b.setXSpeed(getXSpeed() / 2);
+            setXSpeed(-getXSpeed());
             b.setXSpeed(-b.getXSpeed());
-            
-            if (yVel ==0) yVel += b.getySpeed()/2;
-            if (b.getySpeed() ==0) b.setySpeed(b.getySpeed() + (int)yVel/2);
-            yVel = - yVel;
-            b.setySpeed(- b.getySpeed());
-            // despegar sprites
-      /*      int cont = 0;
-            while (b.getArea().overlaps(spr.getBoundingRectangle()) && cont<xVel) {
-               spr.setX(spr.getX()+Math.signum(xVel));
-            }   */
-        	//actualizar vidas y herir
-            vidas--;
+
+            if (getYSpeed() == 0) setYSpeed(b.getYSpeed() / 2);
+            if (b.getYSpeed() == 0) b.setYSpeed(getYSpeed() / 2);
+            setYSpeed(-getYSpeed());
+            b.setYSpeed(-b.getYSpeed());
+
+            // Actualizar vidas y estado de daño
+            if (vidas > 0) {
+                vidas--;
+            }
             herido = true;
-  		    tiempoHerido=tiempoHeridoMax;
-  		    sonidoHerido.play();
-            if (vidas<=0) 
-          	    destruida = true; 
+            tiempoHerido = tiempoHeridoMax;
+            sonidoHerido.play();
+            if (vidas <= 0) {
+                destruida = true;
+            }
             return true;
         }
         return false;
     }
-    
     public boolean estaDestruido() {
-       return !herido && destruida;
+        return destruida;
     }
+
     public boolean estaHerido() {
- 	   return herido;
+        return herido;
     }
-    
-    public int getVidas() {return vidas;}
-    //public boolean isDestruida() {return destruida;}
-    public int getX() {return (int) spr.getX();}
-    public int getY() {return (int) spr.getY();}
-	public void setVidas(int vidas2) {vidas = vidas2;}
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(int vidas2) {
+        vidas = vidas2;
+    }
 }
